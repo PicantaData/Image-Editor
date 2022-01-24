904b97d599e360bf320eee767741ef929c4fb3c7
Vrishin Shah <vrishinshah02@gmail.com>
2022-01-24 22:45:17 +0530

    Setting_up_tools_with_label_and_canvas_method

--- test.py
+++ test.py
@@ -25,7 +25,11 @@ frame_left.grid(row=0,column=0)
 frame_left.grid_propagate(0)
 
 #LEFT-FRAME_CANVAS
-img_canvas = Canvas(frame_left,width=(0.95*f_left_width),height=(0.9*f_left_height), bg="white")
+global canvas_width
+global canvas_height
+canvas_width = int(0.95*f_left_width)
+canvas_height = int(0.9*f_left_height)
+img_canvas = Canvas(frame_left,width=canvas_width,height=canvas_height, bg="white")
 img_canvas.grid(row=0,column=0,pady=5)
 #img_canvas.place(relx=0.5,anchor=N)
 img_canvas.grid_propagate(0)
@@ -50,16 +54,27 @@ frame_right.grid_propagate(0)
 
 #IMPORT-BUTTON
 def import_img():
-    global open_image
-    global open_image_tkinter
-    global open_image_label
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
     
     frame_left.imgaddress = filedialog.askopenfilename(initialdir="/",title="Select an image",filetypes=(("jpg files","*.jpg"),("png files", "*.png"),("jpeg files","*.jpeg")))
+    pillow_image = Image.open(frame_left.imgaddress)
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+
+    #Label mehod
     #open_image = ImageTk.PhotoImage(Image.open(frame_left.imgaddress))
-    open_image = Image.open(frame_left.imgaddress)
-    open_image_tkinter = ImageTk.PhotoImage(open_image)
-    open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    #open_image_label = Label(img_canvas,image= open_image_canvas)
+    #open_image_label.grid(row=0,column=0,padx=10,pady=10)
       
 insert_img_button = Button(frame_right, text="Import Image",command = import_img,bg="black",fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 insert_img_button.grid(row=0,column=0,columnspan=2,padx=0.35*f_right_width,pady=20)
@@ -67,70 +82,147 @@ insert_img_button.grid(row=0,column=0,columnspan=2,padx=0.35*f_right_width,pady=
 
 #EDITING-BUTTONS
 def rotate_right90():
-    global open_image
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
+    
+    pillow_image = pillow_image.rotate(270)
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+    '''global open_image
     global open_image_tkinter
     global open_image_label
     
     open_image = open_image.rotate(270)#, Image.NEAREST, expand = 1)
     open_image_tkinter = ImageTk.PhotoImage(open_image)
     open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    open_image_label.grid(row=0,column=0,padx=10,pady=10)'''
 
 rotate_right = Button(frame_right,text="Rotate Right",bg="black",command=rotate_right90,fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 rotate_right.grid(row=1,column=1,padx=10,pady=10)
 #rotate_right.place(relx=0.8,y=80,anchor=E)
 
 def rotate_left90():
-    global open_image
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
+    
+    pillow_image = pillow_image.rotate(90)
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+
+
+    ''' global open_image
     global open_image_tkinter
     global open_image_label
     
     open_image = open_image.rotate(90)#, Image.NEAREST, expand = 1)
     open_image_tkinter = ImageTk.PhotoImage(open_image)
     open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    open_image_label.grid(row=0,column=0,padx=10,pady=10) '''
 
 rotate_left = Button(frame_right,text="Rotate Left",bg="black",command=rotate_left90,fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 rotate_left.grid(row=1,column=0,padx=10,pady=10)
 #rotate_left.place(relx=0.2,y=80,anchor=W)
 
 def flip_left_right():
-    global open_image
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
+    
+    pillow_image = pillow_image.transpose(Image.FLIP_LEFT_RIGHT)
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+    '''global open_image
     global open_image_tkinter
     global open_image_label
     
     open_image = open_image.transpose(Image.FLIP_LEFT_RIGHT)
     open_image_tkinter = ImageTk.PhotoImage(open_image)
     open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    open_image_label.grid(row=0,column=0,padx=10,pady=10)'''
 
 rotate_left = Button(frame_right,text="Flip Left-Right",bg="black",command=flip_left_right,fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 rotate_left.grid(row=2,column=0,padx=10,pady=10)
 #rotate_left.place(relx=0.2,y=120,anchor=W)
 
 def flip_top_bottom():
-    global open_image
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
+    
+    pillow_image = pillow_image.transpose(Image.FLIP_TOP_BOTTOM)
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+    '''global open_image
     global open_image_tkinter
     global open_image_label
     
     open_image = open_image.transpose(Image.FLIP_TOP_BOTTOM)
     open_image_tkinter = ImageTk.PhotoImage(open_image)
     open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    open_image_label.grid(row=0,column=0,padx=10,pady=10)'''
 
 rotate_right = Button(frame_right,text="Flip Top-Bottom",bg="black",command=flip_top_bottom,fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 rotate_right.grid(row=2,column=1,padx=10,pady=10)
 #rotate_right.place(relx=0.8,y=120,anchor=E)
 
 def greyscale():
-    global open_image
+    global pillow_image
+    global image_tk
+    global resized_image_tk
+    global image_on_canvas
+    global canvas_width
+    global canvas_height
+    
+    pillow_image = pillow_image.convert('L')
+    image_tk = ImageTk.PhotoImage(pillow_image)
+    #size_image = list(image_tk.size())
+    if (image_tk.width!= canvas_width or image_tk.height!=canvas_height):
+        resized_image_tk = ImageTk.PhotoImage(pillow_image.resize((canvas_width,canvas_height)))
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=resized_image_tk)
+    else:
+        image_on_canvas = img_canvas.create_image(0,0,anchor=NW,image=image_tk)
+    '''global open_image
     global open_image_tkinter
     global open_image_label
     
     open_image = open_image.convert('L')
     open_image_tkinter = ImageTk.PhotoImage(open_image)
     open_image_label = Label(img_canvas,image= open_image_tkinter)
-    open_image_label.grid(row=0,column=0,padx=10,pady=10)
+    open_image_label.grid(row=0,column=0,padx=10,pady=10)'''
 
 rotate_right = Button(frame_right,text="Convert to Black & White",bg="black",command=greyscale,fg="white",activebackground="white",activeforeground="black",cursor="hand2",padx=10,pady=5,relief=RAISED)
 rotate_right.grid(row=3,columnspan=2,padx=10,pady=10)
